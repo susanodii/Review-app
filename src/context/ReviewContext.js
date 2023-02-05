b9cba0088ca4d40f3f5b921fdd55f8c0ea8f6951
susan <odiisusan@gmail.com>
2023-02-05 08:49:30 +0100

    Post and Get to dbjson

--- src/context/ReviewContext.js
+++ src/context/ReviewContext.js
@@ -1,45 +1,62 @@
-import {createContext, useEffect, useState} from 'react'
+import { createContext, useEffect, useState } from 'react'
 
-import {v4 as uuidv4} from 'uuid'
+// import {v4 as uuidv4} from 'uuid'
 
 // import reviewData from '../components/data/reviewData'
 
-
 const ReviewContext = createContext()
 
-export const ReviewProvider = ({children}) => {
-  
+export const ReviewProvider = ({ children }) => {
   // const [review, setReview] = useState(reviewData)//data comming from default preview datata
-  const [review, setReview] = useState([])// data from mock server
-   const [isLoading, setIsLoadding] = useState(true)
+  const [review, setReview] = useState([]) // data from mock server
+  const [isLoading, setIsLoadding] = useState(true)
   useEffect(() => {
-      fetch('http://localhost:5000/review')
-      .then(res => res.json())
-      .then(data => {
+    fetch('/review')
+      .then((res) => res.json())
+      .then((data) => {
         setReview(data)
         setIsLoadding(false)
       })
+  }, [])
+  const addReview = async (newReview) => {
+    // newReview.id = uuidv4()// removed this to due to our mock server
+    const res = await fetch('/review', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newReview),
     })
-const addReview = (newReview) => {
-    newReview.id = uuidv4()
-    setReview([newReview, ...review])
+  const data = await res.json()
+    setReview([data, ...review])
   }
 
-const deleteReview = (id) => {
-    if(window.confirm('Are you sure you want to delete this review from the review list?')){
-    setReview(review.filter((item) => item.id !== id))
+  const deleteReview = async (id) => {
+    if (
+      window.confirm('Are you sure you want to delete this review from the review list?')
+     
+      
+    ) { 
+      await fetch(`/review/${id}`, {
+        method : 'DELETE'
+      })
+
+      setReview(review.filter((item) => item.id !== id))
     }
   }
 
-    return <ReviewContext.Provider value={{
+  return (
+    <ReviewContext.Provider
+      value={{
         review,
         deleteReview,
         addReview,
-        isLoading
-    }}>
-            {children}
+        isLoading,
+      }}
+    >
+      {children}
     </ReviewContext.Provider>
+  )
 }
-
 
 export default ReviewContext
